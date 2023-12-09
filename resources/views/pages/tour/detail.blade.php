c811befb1dff930717d939e5aa3f8966540f1e85
urz14x <ustamirazib45@gmail.com>
2023-12-09 14:23:39 +0700

    fix font family dan menambahkan detail tour

---
+++ resources/views/pages/tour/detail.blade.php
@@ -0,0 +1,100 @@
+<x-app-layout title="Kontak Kami | M-One Travella">
+    <section class="min-h-screen">
+        <section class="h-1/2 flex justify-center flex-col bg-white">
+            <div class="py-36 flex items-center justify-center  bg-clr-primary-500">
+                <div class="p-5 md:w-1/2 flex  justify-start flex-col  gap-5">
+                    <h1 class=" text-4xl md:text-5xl text-clr-smooth-white font-bold pt-2">Paket wisata
+                        bandung
+                    </h1>
+                    <p class="text-clr-smooth-white text-sm">Tak terasa, keajaiban Bandung menanti untuk
+                        dijelajahi!
+                        Nikmati pesona alamnya yang memukau, cicipi kuliner khasnya yang lezat, dan rasakan hangatnya
+                        sapaan
+                        ramah penduduk lokal. Ayo, jadikan momen-momen tak terlupakan di Bandung sebagai bagian dari
+                        petualangan seru kita bersama!
+                    </p>
+                </div>
+
+            </div>
+            <section class="container mx-auto">
+                <div class="container  mx-auto px-4 md:px-12 ">
+                    <div class="sm:flex justify-center grid place-items-center flex-wrap mb-5 -mx-1 lg:-mx-4">
+                        <!-- Column -->
+                        <div class="my-1 px-1 w-72 md:w-72 lg:my-4 lg:px-4 lg:w-72">
+                            <!-- Article -->
+                            <article class="overflow-hidden rounded-lg">
+                                <a href="#">
+                                    <img alt="Placeholder" class="block h-auto w-72"
+                                        src="{{ asset('image/paket-info/paket-tb.jpeg') }}">
+                                </a>
+                            </article>
+                            <!-- END Article -->
+                        </div>
+                        <!-- END Column -->
+                        <!-- Column -->
+                        <div class="my-1 px-1 w-72 md:w-72 lg:my-4 lg:px-4 lg:w-72">
+                            <!-- Article -->
+                            <article class="overflow-hidden rounded-lg shadow-lg">
+                                <a href="#">
+                                    <img alt="Placeholder" class="block h-auto w-72"
+                                        src="{{ asset('image/paket-info/paket-tlb.jpeg') }}">
+                                </a>
+                            </article>
+                            <!-- END Article -->
+                        </div>
+                        <!-- END Column -->
+                        <!-- Column -->
+                        <div class="my-1 px-1 w-72 md:w-72 lg:my-4 lg:px-4 lg:w-72">
+                            <!-- Article -->
+                            <article class="overflow-hidden rounded-lg shadow-lg">
+                                <a href="#">
+                                    <img alt="Placeholder" class="block h-auto w-72"
+                                        src="{{ asset('image/paket-info/paket-fo.jpeg') }}">
+                                </a>
+
+                            </article>
+                            <!-- END Article -->
+                        </div>
+                        <!-- END Column -->
+                        <!-- Column -->
+                        <div class="my-1 px-1 w-72 md:w-72 lg:my-4 lg:px-4 lg:w-72">
+                            <!-- Article -->
+                            <article class="overflow-hidden rounded-lg shadow-lg">
+                                <a href="#">
+                                    <img alt="Placeholder" class="block h-auto w-72"
+                                        src="{{ asset('image/paket-info/paket-tbs-1.jpeg') }}">
+                                </a>
+                            </article>
+                            <!-- END Article -->
+                        </div>
+                        <!-- END Column -->
+                        <!-- Column -->
+                        <div class="my-1 px-1 w-72 md:w-72 lg:my-4 lg:px-4 lg:w-72">
+                            <!-- Article -->
+                            <article class="overflow-hidden rounded-lg shadow-lg">
+                                <a href="#">
+                                    <img alt="Placeholder" class="block h-auto w-72"
+                                        src="{{ asset('image/paket-info/paket-rp-1.jpeg') }}">
+                                </a>
+                            </article>
+                            <!-- END Article -->
+                        </div>
+                        <!-- END Column -->
+                        <!-- Column -->
+                        <div class="my-1 px-1 w-72 md:w-72 lg:my-4 lg:px-4 lg:w-72">
+                            <!-- Article -->
+                            <article class="overflow-hidden rounded-lg shadow-lg">
+                                <a href="#">
+                                    <img alt="Placeholder" class="block h-auto w-72"
+                                        src="{{ asset('image/paket-info/paket-rp-2.jpeg') }}">
+                                </a>
+                            </article>
+                            <!-- END Article -->
+                        </div>
+                        <!-- END Column -->
+                    </div>
+                </div>
+            </section>
+        </section>
+    </section>
+</x-app-layout>
